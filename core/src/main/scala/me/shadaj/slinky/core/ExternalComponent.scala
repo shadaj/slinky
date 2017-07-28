e5eb0db5a560368f2daa4e197e3cb9bfc0cf04cb
Shadaj Laddad <shadaj@meteor.com>
2017-07-28 15:41:17 -0700

    Add macro hack to make no-children compile in IntelliJ and SBT

--- core/src/main/scala/me/shadaj/slinky/core/ExternalComponent.scala
+++ core/src/main/scala/me/shadaj/slinky/core/ExternalComponent.scala
@@ -3,10 +3,13 @@ package me.shadaj.slinky.core
 import me.shadaj.slinky.core.facade.{ComponentInstance, React}
 
 import scala.language.implicitConversions
+import scala.language.experimental.macros
+
+import scala.reflect.macros.blackbox.Context
 import scala.scalajs.js
 
-class BuildingComponent[Props](e: ExternalComponent, props: Props, key: String, ref: js.Object => Unit, writer: Writer[Props]) {
-  def apply(children: ComponentInstance*): ComponentInstance = {
+class BuildingComponent[P](e: ExternalComponent, props: P, key: String, ref: js.Object => Unit) {
+  def apply(children: ComponentInstance*)(implicit writer: Writer[P]): ComponentInstance = {
     val written = writer.write(props)
 
     if (key != null) {
@@ -22,17 +25,26 @@ class BuildingComponent[Props](e: ExternalComponent, props: Props, key: String, 
 }
 
 object BuildingComponent {
-  implicit def shortcut(bc: BuildingComponent[_]): ComponentInstance = {
-    bc()
+  implicit def make[P]: BuildingComponent[P] => ComponentInstance = macro BuildingComponentMacros.makeImpl[P]
+}
+
+object BuildingComponentMacros {
+  // SUPER SKETCHY INTELLIJ HACK
+  def makeImpl[P: c.WeakTypeTag](c: Context): c.Expr[BuildingComponent[P] => ComponentInstance] = {
+    import c.universe._
+    val propsType = implicitly[c.WeakTypeTag[P]].tpe
+    c.Expr[BuildingComponent[P] => ComponentInstance](
+      q"(bc: BuildingComponent[$propsType]) => bc.apply()(_root_.scala.Predef.implicitly[_root_.me.shadaj.slinky.core.Writer[$propsType]])"
+    )
   }
 }
 
-trait ExternalComponent {
+abstract class ExternalComponent {
   type Props
 
   val component: js.Object
 
-  def apply(p: Props, key: String = null, ref: js.Object => Unit = null)(implicit writer: Writer[Props]): BuildingComponent[Props] = {
-    new BuildingComponent[Props](this, p, key, ref, writer)
+  def apply(p: Props, key: String = null, ref: js.Object => Unit = null): BuildingComponent[Props] = {
+    new BuildingComponent(this, p, key, ref)
   }
 }
