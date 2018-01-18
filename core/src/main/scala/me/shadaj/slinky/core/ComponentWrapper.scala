295f6db027a2d5924a471bd38f1c5e3ca7208849
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-01-18 10:25:50 -0800

    Add support for exporting StatelessComponent
    Fix #98

--- core/src/main/scala/me/shadaj/slinky/core/ComponentWrapper.scala
+++ core/src/main/scala/me/shadaj/slinky/core/ComponentWrapper.scala
@@ -1,23 +1,7 @@
 package me.shadaj.slinky.core
 
-import me.shadaj.slinky.readwrite.{Reader, Writer}
-
-import scala.scalajs.js
-import scala.scalajs.js.ConstructorTag
 import scala.language.implicitConversions
 
 abstract class ComponentWrapper(implicit pr: PropsReaderProvider, pw: PropsWriterProvider, sr: StateReaderProvider, sw: StateWriterProvider) extends BaseComponentWrapper(pr, pw, sr, sw) {
   override type Definition = DefinitionBase[Props, State]
-}
-
-@js.native
-trait ReactComponentClass extends js.Object
-
-object ReactComponentClass {
-  implicit def wrapperToClass[T <: ComponentWrapper](wrapper: T)
-                                                    (implicit propsReader: Reader[T#Props],
-                                                     stateWriter: Writer[T#State], stateReader: Reader[T#State],
-                                                     ctag: ConstructorTag[wrapper.Def]): ReactComponentClass = {
-    wrapper.componentConstructor.asInstanceOf[ReactComponentClass]
-  }
 }
---
+++ tests/src/test/scala/me/shadaj/slinky/core/ExportedComponentTest.scala
@@ -0,0 +1,45 @@
+package me.shadaj.slinky.core
+
+import me.shadaj.slinky.core.facade.{React, ReactElement}
+import me.shadaj.slinky.web.ReactDOM
+import org.scalatest.FunSuite
+
+import scala.scalajs.js
+import org.scalajs.dom.document
+
+object TestExportedComponentWithState extends ComponentWrapper {
+  case class Props(name: String)
+  type State = Int
+
+  class Def(jsProps: js.Object) extends Definition(jsProps) {
+    override def initialState: Int = 1
+
+    override def render(): ReactElement = {
+      s"${props.name} $state"
+    }
+  }
+}
+
+object TestExportedComponentStateless extends StatelessComponentWrapper {
+  case class Props(name: String)
+
+  class Def(jsProps: js.Object) extends Definition(jsProps) {
+    override def render(): ReactElement = {
+      s"${props.name}"
+    }
+  }
+}
+
+class ExportedComponentTest extends FunSuite {
+  test("Can construct an instance of an exported component with JS-provided props") {
+    val container = document.createElement("div")
+    ReactDOM.render(React.createElement(
+      TestExportedComponentWithState: ReactComponentClass,
+      js.Dictionary(
+        "name" -> "lol"
+      )
+    ), container)
+
+    assert(container.innerHTML == "lol 1")
+  }
+}
