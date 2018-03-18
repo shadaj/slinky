a6d20851dc7fd3e484655c0558ba443d7a21bbe0
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-03-17 18:51:54 -0700

    Add Clipboard APIs

--- native/src/test/scala/slinky/native/NativeStaticAPITest.scala
+++ native/src/test/scala/slinky/native/NativeStaticAPITest.scala
@@ -2,8 +2,18 @@ package slinky.native
 
 import org.scalatest.FunSuite
 
+import scala.scalajs.js
+
 class NativeStaticAPITest extends FunSuite {
   test("Can fire an alert") {
     Alert.alert("alert!")
+  }
+
+  test("Can read clipboard value") {
+    assert(!js.isUndefined(Clipboard.getString))
+  }
+
+  test("Can write clipboard value") {
+    Clipboard.setString("bar")
   }
 }
