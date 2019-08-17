515c70adbf198d28d26d7fa91bddd9c9a5edb3e3
Shadaj Laddad <shadaj@users.noreply.github.com>
2019-08-16 19:08:57 -0700

    Add publishing setup

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 set -e # exit with nonzero exit code if anything fails
 
-openssl aes-256-cbc -K $encrypted_6c42794c35b3_key -iv $encrypted_6c42794c35b3_iv -in secrets.tar.enc -out secrets.tar -d
+openssl aes-256-cbc -K $encrypted_key -iv $encrypted_iv -in secrets.tar.enc -out secrets.tar -d
 
 tar xvf secrets.tar
 
