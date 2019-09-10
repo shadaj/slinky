9be4547d55bcfdfa97bf7c6616176bcaa0e8b0c3
Zak <pattersonzak@gmail.com>
2019-09-09 19:50:37 -0500

    Use js.Function0[A] instead of () => A, per review

--- native/src/main/scala/slinky/native/Keyboard.scala
+++ native/src/main/scala/slinky/native/Keyboard.scala
@@ -6,9 +6,9 @@ import scala.scalajs.js.annotation.JSImport
 @js.native
 @JSImport("react-native", "Keyboard")
 object Keyboard extends js.Object {
-  def addListener(eventName: String, callBack: () => Unit): Unit = js.native
+  def addListener(eventName: String, callBack: js.Function0[Unit]): Unit = js.native
 
-  def removeListener(eventName: String, callBack: () => Unit): Unit = js.native
+  def removeListener(eventName: String, callBack: js.Function0[Unit]): Unit = js.native
 
   def removeAllListeners(eventName: String): Unit = js.native
 
