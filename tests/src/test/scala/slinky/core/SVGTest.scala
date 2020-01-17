77fa252ea752755252ba42faa777f2918a837b1e
Seamus Venasse <svenasse@gmail.com>
2020-01-16 19:41:08 -0800

    Add className and role attributes to svg (#314)
    * Add className and role attributes to svg
    
    * Added unit tests
    
    * Updated CHANGELOG.md
    
    * Point to PR and not issue

--- tests/src/test/scala/slinky/core/SVGTest.scala
+++ tests/src/test/scala/slinky/core/SVGTest.scala
@@ -1,7 +1,7 @@
 package slinky.core
 
-import slinky.core.facade.ReactElement
 import org.scalatest.FunSuite
+import slinky.core.facade.ReactElement
 import slinky.web.svg._
 
 import scala.scalajs.js
@@ -11,5 +11,17 @@ class SVGTest extends FunSuite {
     val instance: ReactElement = circle(key := "1")
 
     assert(instance.asInstanceOf[js.Dynamic].key.asInstanceOf[String] == "1")
+  }
+
+  test("Can specify className attribute for SVG element") {
+    val instance: ReactElement = svg(className := "foo")
+
+    assert(instance.asInstanceOf[js.Dynamic].props.className.asInstanceOf[String] == "foo")
+  }
+
+  test("Can specify role attribute for SVG element") {
+    val instance: ReactElement = svg(role := "button")
+
+    assert(instance.asInstanceOf[js.Dynamic].props.role.asInstanceOf[String] == "button")
   }
 }
