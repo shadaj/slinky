2131945ade3d89cd9fa2d47ea910736af158e672
mario <mario.callisto@gmail.com>
2020-01-30 19:52:29 +0100

    Add style prop to ScrollView, as already available for View

--- native/src/main/scala/slinky/native/ScrollView.scala
+++ native/src/main/scala/slinky/native/ScrollView.scala
@@ -14,57 +14,56 @@ case class ScrollOptions(animated: Boolean)
 
 @js.native
 trait ScrollViewInstance extends js.Object {
-  def scrollTo(target: ObjectOrWritten[ScrollTarget]): Unit      = js.native
-  def scrollToEnd(): Unit                                        = js.native
+  def scrollTo(target: ObjectOrWritten[ScrollTarget]): Unit = js.native
+  def scrollToEnd(): Unit = js.native
   def scrollToEnd(options: ObjectOrWritten[ScrollOptions]): Unit = js.native
-  def flashScrollIndicators(): Unit                              = js.native
+  def flashScrollIndicators(): Unit = js.native
 }
 
 @react object ScrollView extends ExternalComponentWithRefType[ScrollViewInstance] {
-  case class Props(
-    alwaysBounceVertical: js.UndefOr[Boolean] = js.undefined,
-    contentContainerStyle: js.UndefOr[js.Object] = js.undefined,
-    keyboardDismissMode: js.UndefOr[String] = js.undefined,
-    keyboardShouldPersistTaps: js.UndefOr[String] = js.undefined,
-    onContentSizeChange: js.UndefOr[(Int, Int) => Unit] = js.undefined,
-    onMomentumScrollBegin: js.UndefOr[() => Unit] = js.undefined,
-    onMomentumScrollEnd: js.UndefOr[() => Unit] = js.undefined,
-    onScroll: js.UndefOr[() => Unit] = js.undefined,
-    onScrollBeginDrag: js.UndefOr[() => Unit] = js.undefined,
-    onScrollEndDrag: js.UndefOr[() => Unit] = js.undefined,
-    pagingEnabled: js.UndefOr[Boolean] = js.undefined,
-    refreshControl: js.UndefOr[ReactElement] = js.undefined,
-    removeClippedSubviews: js.UndefOr[Boolean] = js.undefined,
-    scrollEnabled: js.UndefOr[Boolean] = js.undefined,
-    showsHorizontalScrollIndicator: js.UndefOr[Boolean] = js.undefined,
-    showsVerticalScrollIndicator: js.UndefOr[Boolean] = js.undefined,
-    stickyHeaderIndices: js.UndefOr[Seq[Int]] = js.undefined,
-    endFillColor: js.UndefOr[String] = js.undefined,
-    overScrollMode: js.UndefOr[String] = js.undefined,
-    scrollPerfTag: js.UndefOr[String] = js.undefined,
-    alwaysBounceHorizontal: js.UndefOr[Boolean] = js.undefined,
-    horizontal: js.UndefOr[Boolean] = js.undefined,
-    automaticallyAdjustContentInsets: js.UndefOr[Boolean] = js.undefined,
-    bounces: js.UndefOr[Boolean] = js.undefined,
-    bouncesZoom: js.UndefOr[Boolean] = js.undefined,
-    canCancelContentTouches: js.UndefOr[Boolean] = js.undefined,
-    centerContent: js.UndefOr[Boolean] = js.undefined,
-    contentInset: js.UndefOr[BoundingBox] = js.undefined,
-    contentInsetAdjustmentBehavior: js.UndefOr[String] = js.undefined,
-    contentOffset: js.UndefOr[ContentOffset] = js.undefined,
-    decelerationRate: js.UndefOr[String | Double] = js.undefined,
-    directionalLockEnabled: js.UndefOr[Boolean] = js.undefined,
-    indicatorStyle: js.UndefOr[String] = js.undefined,
-    maximumZoomScale: js.UndefOr[Double] = js.undefined,
-    minimumZoomScale: js.UndefOr[Double] = js.undefined,
-    pinchGestureEnabled: js.UndefOr[Boolean] = js.undefined,
-    scrollEventThrottle: js.UndefOr[Int] = js.undefined,
-    scrollIndicatorInsets: js.UndefOr[BoundingBox] = js.undefined,
-    scrollsToTop: js.UndefOr[Boolean] = js.undefined,
-    snapToAlignment: js.UndefOr[String] = js.undefined,
-    snapToInterval: js.UndefOr[Double] = js.undefined,
-    zoomScale: js.UndefOr[Double] = js.undefined
-  )
+  case class Props(alwaysBounceVertical: js.UndefOr[Boolean] = js.undefined,
+                   contentContainerStyle: js.UndefOr[js.Object] = js.undefined,
+                   keyboardDismissMode: js.UndefOr[String] = js.undefined,
+                   keyboardShouldPersistTaps: js.UndefOr[String] = js.undefined,
+                   onContentSizeChange: js.UndefOr[(Int, Int) => Unit] = js.undefined,
+                   onMomentumScrollBegin: js.UndefOr[() => Unit] = js.undefined,
+                   onMomentumScrollEnd: js.UndefOr[() => Unit] = js.undefined,
+                   onScroll: js.UndefOr[() => Unit] = js.undefined,
+                   onScrollBeginDrag: js.UndefOr[() => Unit] = js.undefined,
+                   onScrollEndDrag: js.UndefOr[() => Unit] = js.undefined,
+                   pagingEnabled: js.UndefOr[Boolean] = js.undefined,
+                   refreshControl: js.UndefOr[ReactElement] = js.undefined,
+                   removeClippedSubviews: js.UndefOr[Boolean] = js.undefined,
+                   style: js.UndefOr[js.Object] = js.undefined,
+                   scrollEnabled: js.UndefOr[Boolean] = js.undefined,
+                   showsHorizontalScrollIndicator: js.UndefOr[Boolean] = js.undefined,
+                   showsVerticalScrollIndicator: js.UndefOr[Boolean] = js.undefined,
+                   stickyHeaderIndices: js.UndefOr[Seq[Int]] = js.undefined,
+                   endFillColor: js.UndefOr[String] = js.undefined,
+                   overScrollMode: js.UndefOr[String] = js.undefined,
+                   scrollPerfTag: js.UndefOr[String] = js.undefined,
+                   alwaysBounceHorizontal: js.UndefOr[Boolean] = js.undefined,
+                   horizontal: js.UndefOr[Boolean] = js.undefined,
+                   automaticallyAdjustContentInsets: js.UndefOr[Boolean] = js.undefined,
+                   bounces: js.UndefOr[Boolean] = js.undefined,
+                   bouncesZoom: js.UndefOr[Boolean] = js.undefined,
+                   canCancelContentTouches: js.UndefOr[Boolean] = js.undefined,
+                   centerContent: js.UndefOr[Boolean] = js.undefined,
+                   contentInset: js.UndefOr[BoundingBox] = js.undefined,
+                   contentInsetAdjustmentBehavior: js.UndefOr[String] = js.undefined,
+                   contentOffset: js.UndefOr[ContentOffset] = js.undefined,
+                   decelerationRate: js.UndefOr[String | Double] = js.undefined,
+                   directionalLockEnabled: js.UndefOr[Boolean] = js.undefined,
+                   indicatorStyle: js.UndefOr[String] = js.undefined,
+                   maximumZoomScale: js.UndefOr[Double] = js.undefined,
+                   minimumZoomScale: js.UndefOr[Double] = js.undefined,
+                   pinchGestureEnabled: js.UndefOr[Boolean] = js.undefined,
+                   scrollEventThrottle: js.UndefOr[Int] = js.undefined,
+                   scrollIndicatorInsets: js.UndefOr[BoundingBox] = js.undefined,
+                   scrollsToTop: js.UndefOr[Boolean] = js.undefined,
+                   snapToAlignment: js.UndefOr[String] = js.undefined,
+                   snapToInterval: js.UndefOr[Double] = js.undefined,
+                   zoomScale: js.UndefOr[Double] = js.undefined)
 
   @js.native
   @JSImport("react-native", "ScrollView")
