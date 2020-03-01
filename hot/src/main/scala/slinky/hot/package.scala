9e542c895b7a98347f9ad8d7d31ee77305953395
Shadaj Laddad <shadaj@users.noreply.github.com>
2020-02-29 19:39:21 -0800

    Store hot reloading proxies in a module to support Scala.js 1.0

--- hot/src/main/scala/slinky/hot/package.scala
+++ hot/src/main/scala/slinky/hot/package.scala
@@ -7,9 +7,15 @@ import scala.scalajs.js
 
 package object hot {
   def initialize(): Unit = {
-    if (js.isUndefined(js.Dynamic.global.proxies)) {
-      js.Dynamic.global.proxies = js.Dynamic.literal()
-    }
+    val dynamicReactProxyModule = ReactProxy.asInstanceOf[js.Dynamic]
+    val proxyObject: js.Dynamic =
+      if (js.isUndefined(dynamicReactProxyModule._proxies)) {
+        val newProxyStore = js.Dynamic.literal()
+        dynamicReactProxyModule._proxies = newProxyStore
+        newProxyStore
+      } else {
+        dynamicReactProxyModule._proxies
+      }
 
     BaseComponentWrapper.insertMiddleware { (constructor, component) =>
       val componentName = component.asInstanceOf[BaseComponentWrapper].getClass.getName
@@ -17,11 +23,11 @@ package object hot {
       if (js.isUndefined(component.asInstanceOf[js.Dynamic]._hot)) {
         component.asInstanceOf[js.Dynamic]._hot = true
 
-        if (js.isUndefined(js.Dynamic.global.proxies.selectDynamic(componentName))) {
-          js.Dynamic.global.proxies.updateDynamic(componentName)(ReactProxy.createProxy(constructor))
+        if (js.isUndefined(proxyObject.selectDynamic(componentName))) {
+          proxyObject.updateDynamic(componentName)(ReactProxy.createProxy(constructor))
         } else {
           val forceUpdate = ReactProxy.getForceUpdate(ReactRaw)
-          js.Dynamic.global.proxies
+          proxyObject
             .selectDynamic(componentName)
             .update(constructor)
             .asInstanceOf[js.Array[js.Object]]
@@ -29,7 +35,7 @@ package object hot {
         }
       }
 
-      js.Dynamic.global.proxies.selectDynamic(componentName).get().asInstanceOf[js.Object]
+      proxyObject.selectDynamic(componentName).get().asInstanceOf[js.Object]
     }
 
     BaseComponentWrapper.enableScalaComponentWriting()
