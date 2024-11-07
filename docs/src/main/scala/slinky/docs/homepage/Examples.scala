91bc76e5120b89f1dba6424434d6218aa682c659
Scala Steward <scala_steward@virtuslab.com>
2024-11-07 19:25:54 +0000

    Reformat with scalafmt 2.7.5
    Executed command: scalafmt --non-interactive

--- docs/src/main/scala/slinky/docs/homepage/Examples.scala
+++ docs/src/main/scala/slinky/docs/homepage/Examples.scala
@@ -10,65 +10,92 @@ import scala.scalajs.js.Dynamic.literal
 @react object Examples {
   val component = FunctionalComponent[Unit] { _ =>
     div(
-      div(style := literal(
-        display = "flex",
-        flexDirection = "row",
-        justifyContent = "space-between",
-        width = "100%",
-        minHeight = "350px",
-        marginTop = "35px"
-      ))(
-        div(style := literal(
-          width = "30%"
-        ))(
+      div(
+        style := literal(
+          display = "flex",
+          flexDirection = "row",
+          justifyContent = "space-between",
+          width = "100%",
+          minHeight = "350px",
+          marginTop = "35px"
+        )
+      )(
+        div(
+          style := literal(
+            width = "30%"
+          )
+        )(
           h3("A Simple Component"),
           p(
             "Just like React, Slinky components implement a ",
             code("render()"),
-            "method that returns what to display based on the input data, but also define a ", code("Props"), " type that defines the input data shape. ",
+            "method that returns what to display based on the input data, but also define a ",
+            code("Props"),
+            " type that defines the input data shape. ",
             "Slinky comes with a tags API for constructing HTML trees that gives a similar experience to other Scala libraries like ScalaTags but also includes additional type-safety requirements. ",
-            "Input data that is passed into the component can be accessed by ", code("render()"), " via ", code("props")
+            "Input data that is passed into the component can be accessed by ",
+            code("render()"),
+            " via ",
+            code("props")
           )
         ),
         div(style := literal(width = "65%", maxHeight = "450px"))(
           CodeExample("slinky.docs.homepage.HelloMessage")
         )
       ),
-      div(style := literal(
-        display = "flex",
-        flexDirection = "row",
-        justifyContent = "space-between",
-        width = "100%",
-        minHeight = "350px",
-        marginTop = "35px"
-      ))(
-        div(style := literal(
-          width = "30%"
-        ))(
+      div(
+        style := literal(
+          display = "flex",
+          flexDirection = "row",
+          justifyContent = "space-between",
+          width = "100%",
+          minHeight = "350px",
+          marginTop = "35px"
+        )
+      )(
+        div(
+          style := literal(
+            width = "30%"
+          )
+        )(
           h3("A Stateful Component"),
           p(
-            "Slinky components, just like React components, can maintain internal state data (accessed with ", code("state"), ").",
-            " When a component's state data changes after an invocation of ", code("setState"), ", the rendered markup will be update by re-invoking ", code("render()"), "."
+            "Slinky components, just like React components, can maintain internal state data (accessed with ",
+            code("state"),
+            ").",
+            " When a component's state data changes after an invocation of ",
+            code("setState"),
+            ", the rendered markup will be update by re-invoking ",
+            code("render()"),
+            "."
           )
         ),
         div(style := literal(width = "65%", maxHeight = "450px"))(
           CodeExample("slinky.docs.homepage.Timer")
         )
       ),
-      div(style := literal(
-        display = "flex",
-        flexDirection = "row",
-        justifyContent = "space-between",
-        width = "100%",
-        minHeight = "350px",
-        marginTop = "35px"
-      ))(
-        div(style := literal(
-          width = "30%"
-        ))(
+      div(
+        style := literal(
+          display = "flex",
+          flexDirection = "row",
+          justifyContent = "space-between",
+          width = "100%",
+          minHeight = "350px",
+          marginTop = "35px"
+        )
+      )(
+        div(
+          style := literal(
+            width = "30%"
+          )
+        )(
           h3("An Application"),
           p(
-            "Using ", code("props"), " and ", code("state"), ", we can put together a small Todo application. This example uses state to track the current list of items as well as the text that the user has entered."
+            "Using ",
+            code("props"),
+            " and ",
+            code("state"),
+            ", we can put together a small Todo application. This example uses state to track the current list of items as well as the text that the user has entered."
           )
         ),
         div(style := literal(width = "65%", maxHeight = "450px"))(
--- generator/src/main/scala/slinky/generator/MDN.scala
+++ generator/src/main/scala/slinky/generator/MDN.scala
@@ -8,85 +8,85 @@ object MDN extends TagsProvider {
   val browser = JsoupBrowser()
 
   val extraAttributes = List(
-    Attr("key") -> "",
+    Attr("key")                                                   -> "",
     Attr("ref", "scala.Function1[org.scalajs.dom.Element, Unit]") -> "",
-    Attr("dangerouslySetInnerHTML", "js.Object") -> "",
-    Attr("suppressContentEditableWarning", "Boolean") -> "",
-    Attr("defaultChecked", "Boolean") -> "",
-    Attr("aria") -> "",
-    Attr("onAbort", "EventHandler") -> "",
-    Attr("onAutoComplete", "EventHandler") -> "",
-    Attr("onAutoCompleteError", "EventHandler") -> "",
-    Attr("onBlur", "EventHandler") -> "",
-    Attr("onCancel", "EventHandler") -> "",
-    Attr("onCanPlay", "EventHandler") -> "",
-    Attr("onCanPlayThrough", "EventHandler") -> "",
-    Attr("onChange", "EventHandler") -> "",
-    Attr("onClick", "EventHandler") -> "",
-    Attr("onClose", "EventHandler") -> "",
-    Attr("onContextMenu", "EventHandler") -> "",
-    Attr("onCueChange", "EventHandler") -> "",
-    Attr("onDoubleClick", "EventHandler") -> "",
-    Attr("onDrag", "EventHandler") -> "",
-    Attr("onDragEnd", "EventHandler") -> "",
-    Attr("onDragEnter", "EventHandler") -> "",
-    Attr("onDragExit", "EventHandler") -> "",
-    Attr("onDragLeave", "EventHandler") -> "",
-    Attr("onDragOver", "EventHandler") -> "",
-    Attr("onDragStart", "EventHandler") -> "",
-    Attr("onDrop", "EventHandler") -> "",
-    Attr("onDurationChange", "EventHandler") -> "",
-    Attr("onEmptied", "EventHandler") -> "",
-    Attr("onEnded", "EventHandler") -> "",
-    Attr("onError", "EventHandler") -> "",
-    Attr("onFocus", "EventHandler") -> "",
-    Attr("onInput", "EventHandler") -> "",
-    Attr("onInvalid", "EventHandler") -> "",
-    Attr("onKeyDown", "EventHandler") -> "",
-    Attr("onKeyPress", "EventHandler") -> "",
-    Attr("onKeyUp", "EventHandler") -> "",
-    Attr("onLoad", "EventHandler") -> "",
-    Attr("onLoadedData", "EventHandler") -> "",
-    Attr("onLoadedMetadata", "EventHandler") -> "",
-    Attr("onLoadStart", "EventHandler") -> "",
-    Attr("onMouseDown", "MouseEventHandler") -> "",
-    Attr("onMouseEnter", "MouseEventHandler") -> "",
-    Attr("onMouseLeave", "MouseEventHandler") -> "",
-    Attr("onMouseMove", "MouseEventHandler") -> "",
-    Attr("onMouseOut", "MouseEventHandler") -> "",
-    Attr("onMouseOver", "MouseEventHandler") -> "",
-    Attr("onMouseUp", "MouseEventHandler") -> "",
-    Attr("onMouseWheel", "MouseEventHandler") -> "",
-    Attr("onPause", "EventHandler") -> "",
-    Attr("onPlay", "EventHandler") -> "",
-    Attr("onPlaying", "EventHandler") -> "",
-    Attr("onProgress", "EventHandler") -> "",
-    Attr("onRateChange", "EventHandler") -> "",
-    Attr("onReset", "EventHandler") -> "",
-    Attr("onResize", "EventHandler") -> "",
-    Attr("onScroll", "EventHandler") -> "",
-    Attr("onSeeked", "EventHandler") -> "",
-    Attr("onSeeking", "EventHandler") -> "",
-    Attr("onSelect", "EventHandler") -> "",
-    Attr("onShow", "EventHandler") -> "",
-    Attr("onSort", "EventHandler") -> "",
-    Attr("onStalled", "EventHandler") -> "",
-    Attr("onSubmit", "EventHandler") -> "",
-    Attr("onSuspend", "EventHandler") -> "",
-    Attr("onTimeUpdate", "EventHandler") -> "",
-    Attr("onToggle", "EventHandler") -> "",
-    Attr("onVolumeChange", "EventHandler") -> "",
-    Attr("onWaiting", "EventHandler") -> "",
-    Attr("onPointerDown", "MouseEventHandler") -> "",
-    Attr("onPointerMove", "MouseEventHandler") -> "",
-    Attr("onPointerUp", "MouseEventHandler") -> "",
-    Attr("onPointerCancel", "MouseEventHandler") -> "",
-    Attr("onGotPointerCapture", "MouseEventHandler") -> "",
-    Attr("onLostPointerCapture", "MouseEventHandler") -> "",
-    Attr("onPointerEnter", "MouseEventHandler") -> "",
-    Attr("onPointerLeave", "MouseEventHandler") -> "",
-    Attr("onPointerOver", "MouseEventHandler") -> "",
-    Attr("onPointerOut", "MouseEventHandler") -> ""
+    Attr("dangerouslySetInnerHTML", "js.Object")                  -> "",
+    Attr("suppressContentEditableWarning", "Boolean")             -> "",
+    Attr("defaultChecked", "Boolean")                             -> "",
+    Attr("aria")                                                  -> "",
+    Attr("onAbort", "EventHandler")                               -> "",
+    Attr("onAutoComplete", "EventHandler")                        -> "",
+    Attr("onAutoCompleteError", "EventHandler")                   -> "",
+    Attr("onBlur", "EventHandler")                                -> "",
+    Attr("onCancel", "EventHandler")                              -> "",
+    Attr("onCanPlay", "EventHandler")                             -> "",
+    Attr("onCanPlayThrough", "EventHandler")                      -> "",
+    Attr("onChange", "EventHandler")                              -> "",
+    Attr("onClick", "EventHandler")                               -> "",
+    Attr("onClose", "EventHandler")                               -> "",
+    Attr("onContextMenu", "EventHandler")                         -> "",
+    Attr("onCueChange", "EventHandler")                           -> "",
+    Attr("onDoubleClick", "EventHandler")                         -> "",
+    Attr("onDrag", "EventHandler")                                -> "",
+    Attr("onDragEnd", "EventHandler")                             -> "",
+    Attr("onDragEnter", "EventHandler")                           -> "",
+    Attr("onDragExit", "EventHandler")                            -> "",
+    Attr("onDragLeave", "EventHandler")                           -> "",
+    Attr("onDragOver", "EventHandler")                            -> "",
+    Attr("onDragStart", "EventHandler")                           -> "",
+    Attr("onDrop", "EventHandler")                                -> "",
+    Attr("onDurationChange", "EventHandler")                      -> "",
+    Attr("onEmptied", "EventHandler")                             -> "",
+    Attr("onEnded", "EventHandler")                               -> "",
+    Attr("onError", "EventHandler")                               -> "",
+    Attr("onFocus", "EventHandler")                               -> "",
+    Attr("onInput", "EventHandler")                               -> "",
+    Attr("onInvalid", "EventHandler")                             -> "",
+    Attr("onKeyDown", "EventHandler")                             -> "",
+    Attr("onKeyPress", "EventHandler")                            -> "",
+    Attr("onKeyUp", "EventHandler")                               -> "",
+    Attr("onLoad", "EventHandler")                                -> "",
+    Attr("onLoadedData", "EventHandler")                          -> "",
+    Attr("onLoadedMetadata", "EventHandler")                      -> "",
+    Attr("onLoadStart", "EventHandler")                           -> "",
+    Attr("onMouseDown", "MouseEventHandler")                      -> "",
+    Attr("onMouseEnter", "MouseEventHandler")                     -> "",
+    Attr("onMouseLeave", "MouseEventHandler")                     -> "",
+    Attr("onMouseMove", "MouseEventHandler")                      -> "",
+    Attr("onMouseOut", "MouseEventHandler")                       -> "",
+    Attr("onMouseOver", "MouseEventHandler")                      -> "",
+    Attr("onMouseUp", "MouseEventHandler")                        -> "",
+    Attr("onMouseWheel", "MouseEventHandler")                     -> "",
+    Attr("onPause", "EventHandler")                               -> "",
+    Attr("onPlay", "EventHandler")                                -> "",
+    Attr("onPlaying", "EventHandler")                             -> "",
+    Attr("onProgress", "EventHandler")                            -> "",
+    Attr("onRateChange", "EventHandler")                          -> "",
+    Attr("onReset", "EventHandler")                               -> "",
+    Attr("onResize", "EventHandler")                              -> "",
+    Attr("onScroll", "EventHandler")                              -> "",
+    Attr("onSeeked", "EventHandler")                              -> "",
+    Attr("onSeeking", "EventHandler")                             -> "",
+    Attr("onSelect", "EventHandler")                              -> "",
+    Attr("onShow", "EventHandler")                                -> "",
+    Attr("onSort", "EventHandler")                                -> "",
+    Attr("onStalled", "EventHandler")                             -> "",
+    Attr("onSubmit", "EventHandler")                              -> "",
+    Attr("onSuspend", "EventHandler")                             -> "",
+    Attr("onTimeUpdate", "EventHandler")                          -> "",
+    Attr("onToggle", "EventHandler")                              -> "",
+    Attr("onVolumeChange", "EventHandler")                        -> "",
+    Attr("onWaiting", "EventHandler")                             -> "",
+    Attr("onPointerDown", "MouseEventHandler")                    -> "",
+    Attr("onPointerMove", "MouseEventHandler")                    -> "",
+    Attr("onPointerUp", "MouseEventHandler")                      -> "",
+    Attr("onPointerCancel", "MouseEventHandler")                  -> "",
+    Attr("onGotPointerCapture", "MouseEventHandler")              -> "",
+    Attr("onLostPointerCapture", "MouseEventHandler")             -> "",
+    Attr("onPointerEnter", "MouseEventHandler")                   -> "",
+    Attr("onPointerLeave", "MouseEventHandler")                   -> "",
+    Attr("onPointerOver", "MouseEventHandler")                    -> "",
+    Attr("onPointerOut", "MouseEventHandler")                     -> ""
   )
 
   val supportedAttributes: Set[String] =
@@ -102,11 +102,13 @@ object MDN extends TagsProvider {
       |optimum pattern placeholder poster preload profile radioGroup readOnly rel
       |required reversed role rowSpan rows sandbox scope scoped scrolling seamless
       |selected shape size sizes span spellCheck src srcDoc srcLang srcSet start step
-      |style summary tabIndex target title type useMap value width wmode wrap"""
-      .stripMargin.split('\n').flatMap(_.split(' ')).toSet
+      |style summary tabIndex target title type useMap value width wmode wrap""".stripMargin
+      .split('\n')
+      .flatMap(_.split(' '))
+      .toSet
 
   lazy val globalAttributes: List[(Attr, String)] = {
-    val page = browser.get(s"https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes")
+    val page    = browser.get(s"https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes")
     val article = page >> element("#wikiArticle")
     val attributesSection = article.children.toList
       .dropWhile(!_.attrs.get("id").contains("Description"))
@@ -114,29 +116,33 @@ object MDN extends TagsProvider {
       .filter(_.tagName == "dl")
       .flatMap(_.children)
 
-    extraAttributes ::: attributesSection.grouped(2).flatMap { i =>
-      val attr = i.head
-      val docs = i.last
+    extraAttributes ::: attributesSection
+      .grouped(2)
+      .flatMap { i =>
+        val attr = i.head
+        val docs = i.last
 
-      val attrString = attr >> text("code")
-      if (extraAttributes.exists(_._1.name.toLowerCase == attrString) || !supportedAttributes.contains(attrString)) {
-        None
-      } else {
-        Some(HTMLToJSMapping.convert(attrString) -> docs.innerHtml)
+        val attrString = attr >> text("code")
+        if (extraAttributes.exists(_._1.name.toLowerCase == attrString) || !supportedAttributes.contains(attrString)) {
+          None
+        } else {
+          Some(HTMLToJSMapping.convert(attrString) -> docs.innerHtml)
+        }
       }
-    }.toList
+      .toList
   }
 
-  def htmlElement(name: String): (String, List[(Attr, String)]) = {
+  def htmlElement(name: String): (String, List[(Attr, String)]) =
     if (Set("h1", "h2", "h3", "h4", "h5", "h6").contains(name)) {
       ("A header element", globalAttributes)
     } else {
-      val page = browser.get(s"https://developer.mozilla.org/en-US/docs/Web/HTML/Element/$name")
+      val page    = browser.get(s"https://developer.mozilla.org/en-US/docs/Web/HTML/Element/$name")
       val article = page >> element("#wikiArticle")
       val summary = article.children.find(c => c.tagName == "p" && c.innerHtml.nonEmpty).get.innerHtml
 
       val attributesSection = article.children.toList
-        .dropWhile(!_.attrs.get("id").contains("Attributes")).tail
+        .dropWhile(!_.attrs.get("id").contains("Attributes"))
+        .tail
         .takeWhile(e => e.tagName != "h2")
         .filter(_.tagName == "dl")
 
@@ -146,7 +152,9 @@ object MDN extends TagsProvider {
           if (cur.tagName == "dt") {
             acc :+ ((cur >> text("code"), ""))
           } else {
-            acc.init :+ acc.last.copy(_2 = if (acc.last._2.isEmpty) cur.innerHtml else acc.last._2 + " " + cur.innerHtml)
+            acc.init :+ acc.last.copy(_2 =
+              if (acc.last._2.isEmpty) cur.innerHtml else acc.last._2 + " " + cur.innerHtml
+            )
           }
         }
 
@@ -161,7 +169,6 @@ object MDN extends TagsProvider {
 
       (summary, attributes)
     }
-  }
 
   val tags: Seq[String] =
     """a abbr address area article aside audio b base bdi bdo big blockquote body br
@@ -171,8 +178,7 @@ object MDN extends TagsProvider {
       |main map mark menu menuitem meta meter nav noscript object ol optgroup option
       |output p param picture pre progress q rp rt ruby s samp script section select
       |small source span strong style sub summary sup table tbody td textarea tfoot th
-      |thead time title tr track u ul var video wbr"""
-    .stripMargin.split('\n').flatMap(_.split(' ')).toSeq
+      |thead time title tr track u ul var video wbr""".stripMargin.split('\n').flatMap(_.split(' ')).toSeq
 
   def extract: (Seq[Tag], Seq[Attribute]) = {
     val tagsWithAttributes = tags.map { n =>
@@ -180,25 +186,28 @@ object MDN extends TagsProvider {
       (Tag(n, "Any", extracted._1.split('\n').toSeq), extracted._2)
     }
 
-    val attrs = tagsWithAttributes.flatMap(v => v._2.map(t => (v._1, t._1, t._2)))
-      .groupBy(_._2).map { case (attr, instances) =>
-      val groupedDocs = instances.groupBy(_._3)
+    val attrs = tagsWithAttributes
+      .flatMap(v => v._2.map(t => (v._1, t._1, t._2)))
+      .groupBy(_._2)
+      .map { case (attr, instances) =>
+        val groupedDocs = instances.groupBy(_._3)
 
-      Attribute(
-        attr.name,
-        attr.valueType,
-        if (groupedDocs.size == 1) {
-          List(groupedDocs.head._1)
-        } else {
-          groupedDocs.toList.map { case (doc, tagsForDoc) =>
-            s"${tagsForDoc.map(_._1.tagName).mkString(", ")} - $doc"
-          }
-        },
-        if (instances.map(_._1.tagName).distinct.size == tags.size) None else Some(instances.map(_._1.tagName)),
-        attr.name == "data" || attr.name == "aria",
-        false
-      )
-    }.toSeq
+        Attribute(
+          attr.name,
+          attr.valueType,
+          if (groupedDocs.size == 1) {
+            List(groupedDocs.head._1)
+          } else {
+            groupedDocs.toList.map { case (doc, tagsForDoc) =>
+              s"${tagsForDoc.map(_._1.tagName).mkString(", ")} - $doc"
+            }
+          },
+          if (instances.map(_._1.tagName).distinct.size == tags.size) None else Some(instances.map(_._1.tagName)),
+          attr.name == "data" || attr.name == "aria",
+          false
+        )
+      }
+      .toSeq
 
     (tagsWithAttributes.map(_._1), attrs)
   }
